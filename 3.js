17a25a8f303b08c285cc4a724803d9758b18b182
Николай <166646063+Waitds@users.noreply.github.com>
2025-08-08 18:39:28 +0500

    Update 3.js

--- 3.js
+++ 3.js
@@ -2,18 +2,18 @@
 function negatori(){
     const img=document.getElementById("niga");
     if (img.src==="https://www.meme-arsenal.com/memes/9cfcee65b81f3d4d1118d57f1b4bf225.jpg"){
-        img.setAttribute("src","https://mass-images.pro/files/preview/3/15/6d991d738790760a1a8dd5ddda1780bf.jpg?1753284656680");       
+        img.setAttribute("src","https://sun9-56.userapi.com/s/v1/if1/JeqKNw7xR-Tvl6ygEYexmJshrh4ZIrJLc0zisLXzuWcIZPbW-5eXyM2axXWz1otk_2vKDTPE.jpg?quality=96&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,1080x1440,1280x1707,1440x1920,1620x2160&from=bu&cs=453x604");       
     }
     else{
         img.setAttribute("src","https://www.meme-arsenal.com/memes/9cfcee65b81f3d4d1118d57f1b4bf225.jpg");
     }
-    
-    const p=document.getElementById("lisi");
+const p=document.getElementById("lisi");
     if (p.textContent==="text about niga aloooooooooooot of"){
         p.textContent="лысый хуесос"
     }
     else{
         p.textContent="text about niga aloooooooooooot of"
     }
- 
+    
+    
 }
