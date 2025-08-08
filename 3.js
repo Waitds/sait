e3a975362716c03f45b5738821fff5ee9381fa54
Николай <166646063+Waitds@users.noreply.github.com>
2025-08-08 17:58:54 +0500

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,11 @@
+
+function negatori(){
+    const img=document.getElementById("niga");
+    if (img.src==="https://www.meme-arsenal.com/memes/9cfcee65b81f3d4d1118d57f1b4bf225.jpg"){
+        img.setAttribute("src","https://mass-images.pro/files/preview/3/15/6d991d738790760a1a8dd5ddda1780bf.jpg?1753284656680");       
+    }
+    else{
+        img.setAttribute("src","https://www.meme-arsenal.com/memes/9cfcee65b81f3d4d1118d57f1b4bf225.jpg");
+    }
+    
+}
